4e540ed75c37816b9769b5022b36e6ae831ee72d
Cesar Mendoza <52425885+mendoce24@users.noreply.github.com>
2024-04-11 17:33:41 +0200

    refactoring game and player

--- Rules/RuleWellTest.cs
+++ Rules/RuleWellTest.cs
@@ -1,4 +1,8 @@
-﻿using GameOfGoose.Factories;
+﻿using GameOfGoose.Board;
+using GameOfGoose.Dice;
+using GameOfGoose.Factories;
+using GameOfGoose.Print;
+using Moq;
 
 namespace GameOfGoose.Tests.Rules
 {
@@ -18,16 +22,59 @@ namespace GameOfGoose.Tests.Rules
             Assert.True(player.InWell);
         }
 
-        [Fact(Skip = "Not implemented yet")]
+        [Fact]
         public void IfPlayerIsStuckInWell_ThenPlayerCannotMove()
         {
-            throw new NotImplementedException();
+            //Arrange
+            var players = new[]
+             {
+                new Player("TestPlayer 1"),
+                new Player("TestPlayer 2")
+            };
+            var mockDice = new Mock<IDice>();
+            var mockPrint = new Mock<IPrint>();
+            BoardGoose board = BoardGoose.Instance;
+            int lastPosition;
+            int[] diceRoll = [1, 1];
+            var game = new Game(players, mockDice.Object, mockPrint.Object, new PrintFormat());
+
+            // Act
+            players[0].MoveTo(29);
+            players[0].Move(diceRoll);
+            lastPosition = players[0].Position;
+            game.PlayTurn(players[0], diceRoll);
+
+            // Assert
+            Assert.True(players[0].InWell);
+            Assert.Equal(lastPosition, players[0].Position);
         }
 
-        [Fact(Skip = "Not implemented yet")]
+        [Fact]
         public void IfPlayerIsStuckInWell_ThenPlayerCannotMove_UntilAnotherPlayersEnters()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var players = new[]
+            {
+                new Player("TestPlayer 1"),
+                new Player("TestPlayer 2")
+            };
+            int[] diceRoll = [1, 1];
+
+            // Act
+            players[0].MoveTo(29);
+            players[0].Move(diceRoll);
+
+            // Assert
+            Assert.True(players[0].InWell);
+
+            // Act
+            players[1].MoveTo(29);
+            players[1].Move(diceRoll);
+            players[0].ValidateWellExit(players);
+
+            // Assert
+            Assert.True(players[1].InWell);
+            Assert.False(players[0].InWell);
         }
     }
 }
